4c9773ce8cb6f831aa0e6ea017cbf78cd5ee0ca2
Waldener Monteiro <jjrr019@gmail.com>
2020-04-13 04:13:05 -0300

    feat: configure redurces

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+// import { combineReducers } from "redux";
+// import * as reducers from "../pages/joker/duck";
+
+// const rootReducer = combineReducers(reducers);
+// export default rootReducer;
+import { combineReducers } from "redux";
+import joker from "../pages/joker/duck";
+export default combineReducers({
+  joker
+});
