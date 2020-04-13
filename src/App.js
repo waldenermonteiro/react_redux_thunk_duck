5a07691e6966a4c90e6da6469a7a6d7e4fc03d93
Waldener Monteiro <jjrr019@gmail.com>
2020-04-13 04:11:49 -0300

    feat: configure App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Joker from "./pages/joker/Joker";
+import Home from "./pages/layouts/Home";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/jokers">Jokers</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route exact path="/jokers">
+              <Joker />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
 }
-
-export default App;
